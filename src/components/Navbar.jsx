9d30f0eb52c71dc350166e681febe0724efe8a29
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-03-31 14:32:36 +0700

    feat(ui): implement navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+import logo from "../assets/react.svg"
+
 export default function Navbar () {
     return (
-        <h1>This is Navbar</h1>
+        <>
+            <header className="navbar-container">
+                <nav>
+                    <img src={logo} alt="react-logo" />
+                    <h1>ReactFacts</h1>
+                </nav>
+            </header>
+        </>
     )
 }
