e9479aa6572cfe558e30d4429dcc4f9a6453c556
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-03-31 15:08:25 +0700

    feat(ui): implement main page and style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,23 @@
+/**
+ * Challenge: Build out the main content section!
+ * 
+ * For now, skip 2 aspects of the design and we'll work on them later:
+ *   1. The colored bullets in the list
+ *   2. The larger gray React logo on the side
+ */
 export default function Main () {
     return (
-        <h1>This is main content</h1>
+        <>
+            <main className="main-container">
+                <h1>Fun facts about React</h1>
+                <ul>
+                    <li>Was first release in 2013</li>
+                    <li>Was originally create by Jordan Walke</li>
+                    <li>Has Well over 200k stars on GitHub</li>
+                    <li>Is maintained by meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </main>
+        </>
     )
 }
